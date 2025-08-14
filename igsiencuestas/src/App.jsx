44542b4095b569a73fe55705fcabaf58d336477c
Josetoanto <jarf1369@gmail.com>
2025-08-14 13:17:46 -0600

    App

--- igsiencuestas/src/App.jsx
+++ igsiencuestas/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:igsiencuestas/src/App.jsx
 import logo from './logo.svg';
 import './App.css';
 import DepartamentosDashboard from './Administrador/components/pages/departamentosDashboard';
@@ -24,16 +23,7 @@ function App() {
         </a>
       </header>
     </div>
-=======
-import { Routes, Route } from 'react-router-dom';
-import DepartamentosDashboard from './Administrador/components/pages/departamentsDashboard';
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<DepartamentosDashboard />} />
-    </Routes>
->>>>>>> Administrador:igsiencuestas/src/App.js
   );
 }
 
