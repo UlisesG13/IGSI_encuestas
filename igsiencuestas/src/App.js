d3e9f9d6bdf75e821935b543c9057cc357170b50
Josetoanto <jarf1369@gmail.com>
2025-08-14 13:15:06 -0600

    Routes

--- igsiencuestas/src/App.js
+++ igsiencuestas/src/App.js
@@ -1,26 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import DepartamentosDashboard from './Administrador/components/pages/departamentosDashboard';
+import { Routes, Route } from 'react-router-dom';
+import DepartamentosDashboard from './Administrador/components/pages/departamentsDashboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <DepartamentosDashboard />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<DepartamentosDashboard />} />
+    </Routes>
   );
 }
 
